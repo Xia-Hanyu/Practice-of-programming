51d9e3a46e2d2fbd15c377ec649536d9d1c35a14
XiaHanyu <62471753+Xia-Hanyu@users.noreply.github.com>
2020-04-04 23:59:18 +0800

    AC

--- enumeration/B:ClockProblem.cpp
+++ enumeration/B:ClockProblem.cpp
@@ -1,3 +1,5 @@
+/*看起来还行的解法，但是TLE
+
 #include<iostream>
 #include<memory>
 #include<cstring>
@@ -107,6 +109,66 @@ int main()
 	}
 	for(int i = 1; i <= 9; ++i)
 		Try(i);
+	system("pause");
+	return 0;
+}
+*/
+
+// 以下正解 虽然很丑
+#include<iostream>
+#include<algorithm>
+#include<cstring>
+using namespace std;
+
+const int inf = 999999999;
+
+int Clock[10]; 
+int op[10]; // 每种操作进行的次数
+int ans[10]; 
+
+int main()
+{
+	int min_cnt = inf;
+	for(int i = 1; i <= 9; ++i){
+		cin >> Clock[i];
+	}
+	// 真正的枚举
+	// 9循环就nm离谱
+	for(op[1] = 0; op[1] < 4; ++op[1])
+	for(op[2] = 0; op[2] < 4; ++op[2])
+	for(op[3] = 0; op[3] < 4; ++op[3])
+	for(op[4] = 0; op[4] < 4; ++op[4])
+	for(op[5] = 0; op[5] < 4; ++op[5])
+	for(op[6] = 0; op[6] < 4; ++op[6])
+	for(op[7] = 0; op[7] < 4; ++op[7])
+	for(op[8] = 0; op[8] < 4; ++op[8])
+	for(op[9] = 0; op[9] < 4; ++op[9])
+	{
+		int sum = 0;
+        sum += (Clock[1] + op[1] + op[2] + op[4]) % 4; // A
+    	sum += (Clock[2] + op[1] + op[2] + op[3] + op[5]) % 4; // B
+    	sum += (Clock[3] + op[2] + op[3] + op[6]) % 4; // C
+        sum += (Clock[4] + op[1] + op [4] + op[5] + op[7]) % 4; // D
+        sum += (Clock[5] + op[1] + op[3] + op[5] + op[7] + op[9]) % 4; // E
+        sum += (Clock[6] + op[3] + op[5] + op[6] + op[9]) % 4; // F
+        sum += (Clock[7] + op[4] + op[7] + op[8]) % 4; // G
+        sum += (Clock[8] + op[5] + op[7] + op[8] + op[9]) % 4; // H
+        sum += (Clock[9] + op[6] + op[8] + op[9]) % 4; // I
+		if(sum == 0){
+			int cnt = 0;
+			for(int i = 1; i <= 9; ++i){
+				cnt += op[i];
+			}
+			if(cnt < min_cnt){
+				min_cnt = cnt;
+				memcpy(ans, op, sizeof(op));
+			}
+		}
+	}
+	for(int i = 1; i <= 9; ++i){
+		while(ans[i]--)
+			cout << i << " ";
+	}
 	system("pause");
 	return 0;
 }
